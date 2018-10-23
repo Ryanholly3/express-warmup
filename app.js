b58419e28f22e0fd256b1f71b456310e8254af6e
ryanholly3 <ryanholly3@gmail.com>
2018-10-23 09:38:23 -0600

    added basic get requests

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
+const cakes = require('./data.json')
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.get('/cakes', (req, res, next) =>{
+  res.json({ cakes })
+})
 
+cakes.map((cake) =>{
+  app.get(`/cake/:id`, (req, res, next) =>{
+    const id = req.params.id
+    res.json({ cake.id })
+  })
+})
 
 app.listen(port, () =>{
   console.log(`You are on ${port} port`)
